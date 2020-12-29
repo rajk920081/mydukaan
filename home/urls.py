c4dec96e71784cb0ea0824abb07472340be4d1ac
rajk920081 <raj.djangoclass@gmail.com>
2020-12-29 09:47:22 +0530

    ajax

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns =[
     path('',views.homepage, name='homepage'),
     path('about/',views.about_views, name='about'),
+    path('getSlider/', views.getSlider, name='getSlider'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
-from products.models import MasterProduct
+from products.models import MasterProduct,MasterProCat
 from .models import MasterSlider
 
 def homepage(request):
     sliders = MasterSlider.objects.filter(status=True)
-    print(sliders)
-    products =MasterProduct.objects.filter()
-    context ={'products':products,'sliders':sliders}
-    return render(request,'home/index.html',context)
+    products = MasterProduct.objects.filter()
+    procat = MasterProCat.objects.filter()
+    context = {'products': products, 'sliders': sliders, 'procat': procat}
+    return render(request,'home/index.html')
 
 def about_views(request):
     return render(request,'home/about.html')
+
+
+from django.shortcuts import HttpResponse
+def getSlider(request):
+    return HttpResponse("hello i m slider function")
+
+
+
