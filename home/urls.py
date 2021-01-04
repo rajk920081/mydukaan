ad1a93011b3bbbce8f90c21e9acb75b97e172ec5
rajk920081 <raj.djangoclass@gmail.com>
2021-01-04 10:02:02 +0530

    with ajx

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns =[
     path('',views.homepage, name='homepage'),
     path('about/',views.about_views, name='about'),
     path('getSlider/', views.getSlider, name='getSlider'),
+    path('getProduct/', views.getProducts, name='getProducts'),
 ]
--- home/views.py
+++ home/views.py
@@ -9,15 +9,38 @@ def homepage(request):
     products = MasterProduct.objects.filter()
     procat = MasterProCat.objects.filter()
     context = {'products': products, 'sliders': sliders, 'procat': procat}
-    return render(request,'home/index.html')
+    return render(request,'home/index.html',context)
 
 def about_views(request):
     return render(request,'home/about.html')
 
 
 from django.shortcuts import HttpResponse
+
 def getSlider(request):
-    return HttpResponse("hello i m slider function")
+    print('abcd helooo')
+    if request.is_ajax():
+        a =request.POST.get('nn1')
+        b =request.POST.get('nn2')
+        print(a,type(a), b , type(b))
+        c =int(a)+int(b)
+        return HttpResponse(c)
+    return HttpResponse("no page found!")
+
+counter =1
+def getProducts(request):
+    global counter
+    products = MasterProduct.objects.filter()
+    if request.is_ajax():
+        vv = request.POST.get('vv')
+        if vv:
+            products = MasterProduct.objects.filter(name__startswith=vv)
+
+    counter =counter +1
+
+    return render(request,'home/products.html',{'products':products,
+                                                'counter':counter
+                                                })
 
 
 
