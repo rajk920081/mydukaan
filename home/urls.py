c45289721539f45f12dcbf31bca9df57aeade7ec
rajk920081 <raj.djangoclass@gmail.com>
2021-01-08 09:48:44 +0530

    details page

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ urlpatterns =[
     path('about/',views.about_views, name='about'),
     path('getSlider/', views.getSlider, name='getSlider'),
     path('getProduct/', views.getProducts, name='getProducts'),
+    path('getCategery/', views.getCategery, name='getCategery'),
+    path('product_details/productid=<str:link>/', views.product_details, name='product_details'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -5,11 +5,7 @@ from products.models import MasterProduct,MasterProCat
 from .models import MasterSlider
 
 def homepage(request):
-    sliders = MasterSlider.objects.filter(status=True)
-    products = MasterProduct.objects.filter()
-    procat = MasterProCat.objects.filter()
-    context = {'products': products, 'sliders': sliders, 'procat': procat}
-    return render(request,'home/index.html',context)
+    return render(request,'home/index.html')
 
 def about_views(request):
     return render(request,'home/about.html')
@@ -18,29 +14,42 @@ def about_views(request):
 from django.shortcuts import HttpResponse
 
 def getSlider(request):
-    print('abcd helooo')
-    if request.is_ajax():
-        a =request.POST.get('nn1')
-        b =request.POST.get('nn2')
-        print(a,type(a), b , type(b))
-        c =int(a)+int(b)
-        return HttpResponse(c)
-    return HttpResponse("no page found!")
+    sliders = MasterSlider.objects.filter(status=True)
+    return render(request, 'home/slider.html', {'sliders': sliders,
+                                                  })
 
-counter =1
+
 def getProducts(request):
-    global counter
-    products = MasterProduct.objects.filter()
-    if request.is_ajax():
-        vv = request.POST.get('vv')
-        if vv:
-            products = MasterProduct.objects.filter(name__startswith=vv)
-
-    counter =counter +1
+    products = MasterProduct.objects.filter().order_by('-pk')
+    # if request.is_ajax():
+    #     vv = request.POST.get('vv')
+    #     if vv:
+    #         products = MasterProduct.objects.filter(name__startswith=vv)
 
     return render(request,'home/products.html',{'products':products,
-                                                'counter':counter
                                                 })
+
+def getCategery(request):
+    categery = MasterProCat.objects.filter().order_by('-pk')
+    return render(request, 'home/categery.html', {'categery': categery,
+                                                  })
+
+
+def product_details(request,link=None):
+    # name =MasterProduct.objects.get() #ORM
+    # onep = MasterProduct.objects.filter(price=500) | MasterProduct.objects.filter(price=250)
+    #     # #filter() # querysert as list
+    #     # #all() # querysert as list
+    #     # #get() # one objects
+    print(link)
+    onep = MasterProduct.objects.get(slug=link)
+    print(onep, type(onep))
+    context ={
+        'onep':onep
+    }
+    return render(request,'home/details.html',context)
+
+
 
 
 
